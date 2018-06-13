ae9c9a6ded89880771f0cae141793e3381e3b2aa
ProsperDev <takashiyoshino720@gmail.com>
2018-06-13 05:56:52 +0200

    automatically disable styles debugger in production, closes #70

--- src/config/debug.js
+++ src/config/debug.js
@@ -1,5 +1,7 @@
 import { CreateStylesDebugger } from 'styles-debugger';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 export const debug = CreateStylesDebugger({
-  enabled: false
+  enabled: !isDev ? false : false
 });
