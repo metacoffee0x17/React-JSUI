e8f3a2813bba8071dd223013f9575b1201397f7e
ProsperDev <takashiyoshino720@gmail.com>
2018-06-30 02:54:23 -0500

    Fixed placeholder absence issue.

--- src/utils/css-to-js.js
+++ src/utils/css-to-js.js
@@ -1,14 +1,23 @@
 import cssToJS from 'transform-css-to-js';
 
 export default input => {
+  // in case of invalid input, output the error
   try {
-    return correctSpacing(cssToJS(input));
+    const jsString = cssToJS(input);
+    const formatted = correctSpacing(jsString);
+    return output(formatted);
   } catch (error) {
     return error.toString();
   }
 };
 
+const output = input => {
+  // default to empty string so that placeholder shows
+  return input.replace(/\s/g, '') === '{}' ? '' : input;
+};
+
 const correctSpacing = target => {
+  // remove weird spacing and give separator lines
   return target
     .replace(/{\n/, '{')
     .replace(/,\n/gm, ',\n\n')
