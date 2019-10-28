82a4273128e6018e51eedc018d1d512e3f86c738
ProsperDev <takashiyoshino720@gmail.com>
2019-10-28 07:47:12 +0100

    remove babel repl custom css

--- src/components/BabelRepl/index.js
+++ src/components/BabelRepl/index.js
@@ -6,16 +6,6 @@ import * as S from './styles';
 class BabelRepl extends Component {
   babelWebView = React.createRef();
 
-  componentDidMount() {
-    const webview = this.babelWebView.current;
-    if (webview && webview.insertCSS) {
-      webview.insertCSS(`
-        * {
-          display: none !important;
-        }
-      `);
-    }
-  }
   render() {
     return <webview ref={this.babelWebView} className={S.BabelRepl} src="https://babeljs.io/repl/" />;
   }
