af73ad055065b20b8c5de28e39f4b286070285aa
ProsperDev <takashiyoshino720@gmail.com>
2019-10-28 09:57:03 +0100

    add after sign script and release script

---
+++ scripts/after-sign.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const { notarize } = require('electron-notarize');
+const { APPLE_ID, APPLE_ID_PASS } = process.env;
+
+module.exports = async function(params) {
+  const { electronPlatformName, appOutDir } = params;
+
+  // Only notarize the app on Mac OS only.
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  console.log('afterSign hook triggered');
+
+  // Same appId in electron-builder.
+  let appId = 'com.kitze.jsui';
+
+  let appPath = path.join(appOutDir, `${params.packager.appInfo.productFilename}.app`);
+
+  if (!fs.existsSync(appPath)) {
+    throw new Error(`Cannot find application at: ${appPath}`);
+  }
+
+  console.log(`Notarizing ${appId} found at ${appPath}`);
+
+  try {
+    const notarizeConfig = {
+      appBundleId: appId,
+      appPath: appPath,
+      appleId: APPLE_ID,
+      appleIdPassword: APPLE_ID_PASS
+    };
+    await notarize(notarizeConfig);
+  } catch (error) {
+    console.error(error);
+  }
+
+  console.log(`Done notarizing ${appId}`);
+};
